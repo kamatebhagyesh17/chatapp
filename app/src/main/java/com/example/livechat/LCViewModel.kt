a5eb80f736709d76fedd59888115c5ddef44d2d1
bmk17 <bhagyeshkamate17@gmail.com>
2023-12-23 11:30:16 +0530

    hii

---
+++ app/src/main/java/com/example/livechat/LCViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.livechat
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LCViewModel @Inject constructor(
+    val auth: FirebaseAuth
+) : ViewModel() {
+    init {
+
+    }
+
+    fun signUp(name: String, number: String, email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+
+
+            if (it.isSuccessful) {
+                Log.d("TAG", "signUp: User Logged In")
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/livechat/Screens/LoginScreen.kt
@@ -0,0 +1,10 @@
+package com.example.livechat.Screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+
+    Text(text = "Login Screen")
+}
